2d5571b0c041efff05c4f62b72ad61f6bae85101
Marian <mancoppor@gmail.com>
2022-06-16 16:55:06 +0200

    Added curve animation JS

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-let a = 5;
+// Background animation
+let curve = document.querySelector(".curve");
+window.addEventListener('mousemove', e => {
+    let x = -(e.x - window.innerWidth / 2) / window.innerWidth * 2;
+    let y = -(e.y - window.innerHeight / 2) / window.innerHeight * 10;
+    curve.style.transform = "translate(" + x + "%, " + y + "%)";
+})
