a45cf60cbc8ab23541b3bde8b4971cc4b4137230
Marian <mancoppor@gmail.com>
2022-06-16 17:12:01 +0200

    Added curve blur

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Background animation
 let curve = document.querySelector(".curve");
 window.addEventListener('mousemove', e => {
-    let x = -(e.x - window.innerWidth / 2) / window.innerWidth * 2;
-    let y = -(e.y - window.innerHeight / 2) / window.innerHeight * 10;
+    let x = -(e.x - window.innerWidth / 2) / window.innerWidth * 12;
+    let y = -(e.y - window.innerHeight / 2) / window.innerHeight * 6;
     curve.style.transform = "translate(" + x + "%, " + y + "%)";
 })
